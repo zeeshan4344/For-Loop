60f4629573d56d78ab16b34465804d9b3ec835bc
Zeeshan Zahid <zeeshanzahid1221@gmail.com>
2024-07-20 23:03:45 +0500

    Break Statement

--- For_Loop.js
+++ For_Loop.js
@@ -4,6 +4,18 @@
 
 
 // Example of For_Loop
-for (let i = 0; i <= 15; i++){
-    console.log(i)
+// for (let i = 0; i <= 15; i++){
+//     console.log(i)
+// }
+
+
+// Break Statement
+for (let i = 1; i <= 15; i++) {
+    if (i == 4) {
+        break;
+    }
+    else {
+        console.log(i);
+    }
+
 }
